82c2f9a926ca81027f19c4795452a542f81658b1
Nevin Tay <nevintay@gmail.com>
2022-11-02 01:40:39 +0800

    initial commit

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+export type IsNumberOrNull = Number | null;
+
+export type Props = {
+    children?: React.ReactNode | React.ReactNode[];
+};
+
+export type CalculatorContextObj = {
+    previousValue?: Number | null;
+    currentValue?: Number | null;
+    pendingValue?: Number | null;
+    handlePreviousValue: (value: IsNumberOrNull) => void;
+    handleCurrentValue: (value: IsNumberOrNull) => void;
+    handlePendingValue: (value: IsNumberOrNull) => void;
+};
