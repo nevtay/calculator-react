fa68762ad85ec3cb53000a718abd48471eed775c
Nevin Tay <nevintay@gmail.com>
2022-11-06 14:14:27 +0800

    setup <Row> display logic

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Props } from "../types/types";
+import Button from "./Button";
 
 const Row = (props: Props) => {
-  return <div>Row</div>;
+  return (
+    <div>
+      {props.values?.map((val) => {
+        return <Button value={val} />;
+      })}
+    </div>
+  );
 };
 
 export default Row;
