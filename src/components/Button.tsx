140cf1094b5ea0e892a9dcda6a93cf435c0d2e4b
Nevin Tay <nevintay@gmail.com>
2022-11-06 15:20:16 +0800

    assign string ids to components that have symbols as value

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,8 +2,22 @@ import "./Button.css";
 import { Props } from "../types/types";
 
 const Button = (props: Props) => {
+  if (props.value === ".") {
+    return (
+      <span id={`btn-point`} className={`btn`}>
+        {props.value}
+      </span>
+    );
+  }
+  if (props.value === "=") {
+    return (
+      <span id={`btn-equal`} className={`btn`}>
+        {props.value}
+      </span>
+    );
+  }
   return (
-    <span className={`button ${props.value === "0" ? "button-0" : ""}`}>
+    <span id={`btn-${props.value}`} className={`btn`}>
       {props.value}
     </span>
   );
