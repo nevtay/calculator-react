c89fa0f4aeb84d076cc0a8df9e365aaee466e14c
Nevin Tay <nevintay@gmail.com>
2022-11-06 14:14:07 +0800

    setup layout of calculator number and symbols

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { Props } from "../types/types";
+import Row from "./Row";
 
 const Display = (props: Props) => {
-  return <div>Display</div>;
+  return (
+    <div>
+      <Row values={["AC", "±", "%", "÷"]} />
+      <Row values={["7", "8", "9", "X"]} />
+      <Row values={["4", "5", "6", "-"]} />
+      <Row values={["1", "2", "3", "+"]} />
+      <Row values={["0", ".", "="]} />
+    </div>
+  );
 };
 
 export default Display;
