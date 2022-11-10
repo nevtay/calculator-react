729ef573394e91fe766590aff9baf7dbc42cb88d
Nevin Tay <nevintay@gmail.com>
2022-11-10 13:10:09 +0800

    display current value from context store

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
+import { CalculatorContext } from "../store/calculator-context";
 import { Props } from "../types/types";
 import "./Output.css";
 
 const Output = (props: Props) => {
+  const ctx = useContext(CalculatorContext);
+  const currentVal = ctx?.currentValue;
   return (
     <div className="output">
-      <p className="currentValue">0</p>
+      <p className="currentValue">{currentVal?.toString() || "0"}</p>
     </div>
   );
 };
