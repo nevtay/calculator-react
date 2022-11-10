79e11238b9150e68306d7286cc01772462098b0a
Nevin Tay <nevintay@gmail.com>
2022-11-10 13:34:12 +0800

    import and assign function from context to button components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { CalculatorContext } from "../store/calculator-context";
 import "./Button.css";
 import { Props } from "../types/types";
 
 const Button = (props: Props) => {
+  const ctx = useContext(CalculatorContext);
+  const handleCurrentValue = ctx?.handleCurrentValue;
+
   if (props.value === ".") {
     return (
       <span id={`btn-point`} className={`btn`}>
@@ -17,7 +22,15 @@ const Button = (props: Props) => {
     );
   }
   return (
-    <span id={`btn-${props.value}`} className={`btn`}>
+    <span
+      id={`btn-${props.value}`}
+      onClick={() => {
+        if (handleCurrentValue) {
+          handleCurrentValue(String(props.value));
+        }
+      }}
+      className={`btn`}
+    >
       {props.value}
     </span>
   );
